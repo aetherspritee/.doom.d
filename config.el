4b05bf064c344bcfcd2513c779c5896db4720670
aetherspritee <schautendustin@gmail.com>
2023-12-23 23:32:02 +0100

    idek why

--- config.el
+++ config.el
@@ -36,7 +36,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-gruvbox)
-(setq doom-theme 'doom-tomorrow-night)
+(setq doom-theme 'doom-one-light)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
